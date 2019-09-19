d0c1528911164a61da838dd149c0f7b7df583038
Jindrich Dolezy <jindrich.dolezy@strv.com>
2019-09-19 21:08:20 +0200

    [FW-robot] Improve OTA logging

--- firmware/robot/x50_ota.ino
+++ firmware/robot/x50_ota.ino
@@ -1,3 +1,4 @@
+uint8_t otaProgress;
 
 void otaSetup() {
   String ssid = HOSTNAME_PREFIX;
@@ -7,10 +8,21 @@ void otaSetup() {
 #ifdef OTA_PASSWORD
   ArduinoOTA.setPassword(OTA_PASSWORD);
 #endif
-  
+
   ArduinoOTA.onStart([]() {
+    logInfo("OTA starting");
     logicSetBrake(1);
-    pixelsSetAnimState(PIXELS_ANIM_GREEN);
+    pixelsSetAnimState(PIXELS_ANIM_GREEN, 3);
+  });
+  ArduinoOTA.onEnd([]() {
+    logInfo("OTA finished");
+  });
+  ArduinoOTA.onProgress([](uint32_t now, uint32_t total) {
+    uint8_t p = map(now, 0, total, 0, 10);
+    if (p != otaProgress) {
+      logValue("OTA: ", p * 10);
+      otaProgress = p;
+    }
   });
   ArduinoOTA.begin();
 }
