b0f96035993a96ce07bfa999a034788685c4ee71
Jindrich Dolezy <jindrich.dolezy@strv.com>
2019-09-20 16:13:07 +0200

    [FW-robot] Adjust pwm min max values and pwm freq

--- firmware/robot/x20_motors.ino
+++ firmware/robot/x20_motors.ino
@@ -7,6 +7,15 @@ uint8_t m2speed;
 uint8_t m1targetSpeed;
 uint8_t m2targetSpeed;
 
+//#define PWM_1_MIN (config.m1maxPwm/4)
+//#define PWM_2_MIN (config.m2maxPwm/4)
+
+#define PWM_1_MIN (config.m1maxPwm/8)
+#define PWM_2_MIN (config.m2maxPwm/8)
+
+#define PWM_1_MAX (config.m1maxPwm)
+#define PWM_2_MAX (config.m2maxPwm)
+
 
 void motorsTick() {
   int delta = constrain((int)m1targetSpeed - (int)m1speed, -MOTORS_ACCEL_PER_TICK, MOTORS_ACCEL_PER_TICK);
@@ -19,18 +28,18 @@ void motorsTick() {
   if (m1speed == 128)
     analogWrite(PIN_M1_EN, 0);
   else
-    analogWrite(PIN_M1_EN, m1speed >= 128 ? map(m1speed, 128, 255, config.m1maxPwm / 4, config.m1maxPwm) : map(m1speed, 127, 0, config.m1maxPwm / 4, config.m1maxPwm));
+    analogWrite(PIN_M1_EN, m1speed >= 128 ? map(m1speed, 128, 255, PWM_1_MIN, PWM_1_MAX) : map(m1speed, 127, 0, PWM_1_MIN, PWM_1_MAX));
 
   digitalWrite(PIN_M2_A, m2speed > 128);
   digitalWrite(PIN_M2_B, m2speed < 128);
   if (m2speed == 128)
     analogWrite(PIN_M2_EN, 0);
   else
-    analogWrite(PIN_M2_EN, m2speed >= 128 ? map(m2speed, 128, 255, config.m2maxPwm / 4, config.m2maxPwm) : map(m2speed, 127, 0, config.m2maxPwm / 4, config.m2maxPwm));
+    analogWrite(PIN_M2_EN, m2speed >= 128 ? map(m2speed, 128, 255, PWM_2_MIN, PWM_2_MAX) : map(m2speed, 127, 0, PWM_2_MIN, PWM_2_MAX));
 }
 
 void motorsSetup() {
-  analogWriteFreq(20000);
+  analogWriteFreq(50);
 
   pinMode(PIN_M1_A, OUTPUT);
   pinMode(PIN_M1_B, OUTPUT);
